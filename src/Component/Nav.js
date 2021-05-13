1f983367a56f10be5b67a2ad879914b94306d21a
Abhineet Verma <198.abhineet@gmail.com>
2021-05-14 01:03:11 +0530

    Fixed Navbar and some minor bugs in UI

--- src/Component/Nav.js
+++ src/Component/Nav.js
@@ -12,178 +12,107 @@ const NavBar = ({ user, userLoggedIn, openMembershipForm }) => {
     document.getElementById("register-tab").className = "nav-link";
   };
   return (
-    <div>
-      <nav className="navbar navbar-expand-lg navbar-dark fixed-top scrolling-navbar">
-        <div className="container-fluid mx-md-5">
-          <a className="navbar-brand" href="/dashboard.html">
-            <strong>UNNAT</strong>
-          </a>
-          <button
-            className="navbar-toggler"
-            type="button"
-            data-toggle="collapse"
-            data-target="#navbarSupportedContent1"
-            aria-controls="navbarSupportedContent1"
-            aria-expanded="false"
-            aria-label="Toggle navigation"
-          >
-            <span className="navbar-toggler-icon"></span>
-          </button>
-          <div
-            className="collapse navbar-collapse"
-            id="navbarSupportedContent1"
-          >
-            <ul className="navbar-nav mr-auto">
-              <li className="nav-item">
-                <a className="nav-link" href="#contact-us">
-                  Contact Us
-                </a>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link" href="#about-us">
-                  About Us
-                </a>
-              </li>
-              <li className="nav-item dropdown">
-                <a
-                  className="nav-link dropdown-toggle"
-                  id="navbarDropdownMenuLink"
-                  data-toggle="dropdown"
-                  aria-expanded="false"
-                  href="#/"
-                >
-                  Members
-                </a>
-                <div
-                  className="dropdown-menu dropdown-primary"
-                  aria-labelledby="navbarDropdownMenuLink"
-                >
-                  <a className="dropdown-item" href="#/">
-                    Technical Team
-                  </a>
-                  <a className="dropdown-item" href="#/">
-                    Documentation Team
-                  </a>
-                  <a className="dropdown-item" href="#/">
-                    Management Team
-                  </a>
-                  <a className="dropdown-item" href="#/">
-                    Designing Team
-                  </a>
-                </div>
-              </li>
-              <li className="nav-item dropdown">
-                <a
-                  className="nav-link dropdown-toggle"
-                  id="navbarDropdownMenuLink"
-                  data-toggle="dropdown"
-                  aria-expanded="false"
-                  href="#/"
-                >
-                  More
-                </a>
-                <div
-                  className="dropdown-menu dropdown-primary"
-                  aria-labelledby="navbarDropdownMenuLink"
-                >
-                  <a className="dropdown-item" href="#teams">
-                    Our Teams
-                  </a>
-                  <a className="dropdown-item" href="#achievements">
-                    Our Achievements
-                  </a>
-                  <a className="dropdown-item" href="#faqs">
-                    FAQs
-                  </a>
-                </div>
-              </li>
-            </ul>
-
-            <ul className="navbar-nav nav-flex-icons ml-auto">
-              <li className="nav-item">
-                <a
-                  className="nav-link"
-                  href="https://m.facebook.com/techclubreck/"
-                  target="_blank"
-                  rel="noreferrer"
-                >
-                  <i className="fab fa-facebook-f" aria-hidden="true"></i>
-                </a>
-              </li>
-              <li className="nav-item">
-                <a
-                  className="nav-link"
-                  href="https://twitter.com/KannaujRec"
-                  target="_blank"
-                  rel="noreferrer"
-                >
-                  <i className="fab fa-twitter" aria-hidden="true"></i>
-                </a>
-              </li>
-              <li className="nav-item">
-                <a
-                  className="nav-link"
-                  href="https://www.instagram.com/abhyutthan.reck/"
-                  target="_blank"
-                  rel="noreferrer"
-                >
-                  <i className="fab fa-instagram" aria-hidden="true"></i>
-                </a>
-              </li>
-            </ul>
-            {user.userRole?.toString() === "" &&
-            !localStorage.getItem("token") ? (
-              <div className="" role="tablist">
-                <a
-                  className="btn btn-sm btn-primary"
-                  href="#account"
-                  onClick={register}
-                >
-                  Register
-                </a>
-                <a
-                  className="btn btn-sm btn-outline-primary"
-                  href="#account"
-                  onClick={login}
-                >
-                  Login
-                </a>
-              </div>
-            ) : (
-              ""
-            )}
-
-            {user.userRole?.toString() === "Member" ||
-            (localStorage.getItem("token") &&
-              localStorage.getItem("role")?.toString() === "Member") ? (
-              <div className="" role="tablist" id="forMember">
-                <a
-                  className="btn btn-sm btn-primary"
-                  href="#account"
-                  onClick={register}
-                >
-                  Club Discussion
-                </a>
-                <a
-                  className="btn btn-sm btn-primary"
-                  href="#account"
-                  onClick={login}
-                >
-                  Announcements/Event Planning
-                </a>
-              </div>
-            ) : (
-              ""
-            )}
-          </div>
-          {user.userRole?.toString() === "Genral User" ||
-          (localStorage.getItem("token") &&
-            localStorage.getItem("role").toString() === "Genral User") ? (
-            <ul style={{ disable: "block", marginTop: "10.5px" }}>
-              <li
-                className="nav-item avatar dropdown "
-                style={{ color: "white" }}
+    <nav className="navbar navbar-expand-lg navbar-dark fixed-top scrolling-navbar">
+      <div className="container-fluid mx-md-5">
+        <a className="navbar-brand" href="/dashboard.html">
+          <strong>UNNAT</strong>
+        </a>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-toggle="collapse"
+          data-target="#navbarSupportedContent1"
+          aria-controls="navbarSupportedContent1"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarSupportedContent1">
+          <ul className="navbar-nav mr-auto">
+            <li className="nav-item">
+              <a className="nav-link" href="#contact-us">
+                Contact Us
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#about-us">
+                About Us
+              </a>
+            </li>
+            <li className="nav-item dropdown">
+              <a
+                className="nav-link dropdown-toggle"
+                id="navbarDropdownMenuLink"
+                data-toggle="dropdown"
+                aria-expanded="false"
+                href="#/"
               >
+                Members
+              </a>
+              <div className="dropdown-menu dropdown-primary" aria-labelledby="navbarDropdownMenuLink">
+                <a className="dropdown-item" href="#/">
+                  Technical Team
+                </a>
+                <a className="dropdown-item" href="#/">
+                  Documentation Team
+                </a>
+                <a className="dropdown-item" href="#/">
+                  Management Team
+                </a>
+                <a className="dropdown-item" href="#/">
+                  Designing Team
+                </a>
+              </div>
+            </li>
+            <li className="nav-item dropdown">
+              <a
+                className="nav-link dropdown-toggle"
+                id="navbarDropdownMenuLink"
+                data-toggle="dropdown"
+                aria-expanded="false"
+                href="#/"
+              >
+                More
+              </a>
+              <div className="dropdown-menu dropdown-primary" aria-labelledby="navbarDropdownMenuLink">
+                <a className="dropdown-item" href="#teams">
+                  Our Teams
+                </a>
+                <a className="dropdown-item" href="#achievements">
+                  Our Achievements
+                </a>
+                <a className="dropdown-item" href="#faqs">
+                  FAQs
+                </a>
+              </div>
+            </li>
+          </ul>
+
+          <ul className="navbar-nav nav-flex-icons ml-auto align-items-center ">
+            <li className="nav-item">
+              <a className="nav-link" href="https://m.facebook.com/techclubreck/" target="_blank" rel="noreferrer">
+                <i className="fab fa-facebook-f" aria-hidden="true"></i>
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="https://twitter.com/KannaujRec" target="_blank" rel="noreferrer">
+                <i className="fab fa-twitter" aria-hidden="true"></i>
+              </a>
+            </li>
+            <li className="nav-item">
+              <a
+                className="nav-link"
+                href="https://www.instagram.com/abhyutthan.reck/"
+                target="_blank"
+                rel="noreferrer"
+              >
+                <i className="fab fa-instagram" aria-hidden="true"></i>
+              </a>
+            </li>
+            {user.userRole?.toString() === "Genral User" ||
+            (localStorage.getItem("token") && localStorage.getItem("role").toString() === "Genral User") ? (
+              <li className="nav-item avatar dropdown ml-1" style={{ color: "white" }}>
                 <a
                   className="nav-link dropdown-toggle p-0 "
                   id="navbarDropdownMenuLink-55"
@@ -193,7 +122,7 @@ const NavBar = ({ user, userLoggedIn, openMembershipForm }) => {
                   style={{ color: "white" }}
                 >
                   <img
-                    src="https://mdbootstrap.com/img/Photos/Avatars/avatar-2.jpg"
+                    src="../img/user.png"
                     className="rounded-circle z-depth-0"
                     alt="User Profile"
                   />
@@ -205,15 +134,8 @@ const NavBar = ({ user, userLoggedIn, openMembershipForm }) => {
                       : ""}
                   </span>
                 </a>
-                <div
-                  className="dropdown-menu dropdown-menu-lg-right p-2"
-                  aria-labelledby="navbarDropdownMenuLink-55"
-                >
-                  <a
-                    className="dropdown-item"
-                    href="#/"
-                    onClick={() => openMembershipForm(true)}
-                  >
+                <div className="dropdown-menu dropdown-menu-lg-right p-2" aria-labelledby="navbarDropdownMenuLink-55">
+                  <a className="dropdown-item" href="#/" onClick={() => openMembershipForm(true)}>
                     Apply For Membership
                   </a>
                   <a className="dropdown-item" href="#/">
@@ -227,23 +149,49 @@ const NavBar = ({ user, userLoggedIn, openMembershipForm }) => {
                     href="#/"
                     onClick={() => {
                       localStorage.clear();
-                      userLoggedIn("userEmail","");
-                      userLoggedIn("userFullName","");
-                      userLoggedIn("userId","");
-                      userLoggedIn("userRole","");
+                      userLoggedIn("userEmail", "");
+                      userLoggedIn("userFullName", "");
+                      userLoggedIn("userId", "");
+                      userLoggedIn("userRole", "");
                     }}
                   >
                     Logout
                   </a>
                 </div>
               </li>
-            </ul>
+            ) : (
+              ""
+            )}
+          </ul>
+          {user.userRole?.toString() === "" && !localStorage.getItem("token") ? (
+            <div className="" role="tablist">
+              <a className="btn btn-sm btn-primary" href="#account" onClick={register}>
+                Register
+              </a>
+              <a className="btn btn-sm btn-outline-primary" href="#account" onClick={login}>
+                Login
+              </a>
+            </div>
+          ) : (
+            ""
+          )}
+
+          {user.userRole?.toString() === "Member" ||
+          (localStorage.getItem("token") && localStorage.getItem("role")?.toString() === "Member") ? (
+            <div className="" role="tablist" id="forMember">
+              <a className="btn btn-sm btn-primary" href="#account" onClick={register}>
+                Club Discussion
+              </a>
+              <a className="btn btn-sm btn-primary" href="#account" onClick={login}>
+                Announcements/Event Planning
+              </a>
+            </div>
           ) : (
             ""
           )}
         </div>
-      </nav>
-    </div>
+      </div>
+    </nav>
   );
 };
 
